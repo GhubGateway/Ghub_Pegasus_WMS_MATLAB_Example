b4b2914ba0387d4105f1fd520ea0db2e0511ea24
Renette Jones-Ivey <2087294+rljbufny1@users.noreply.github.com>
2023-02-21 18:50:32 -0500

    Add files via upload

--- remotebin/matlabLaunch.sh
+++ remotebin/matlabLaunch.sh
@@ -1,1 +1,23 @@
+#!/bin/sh
 
+# Executable script for the ismip6aissvn Pegasus workflow.
+# Called with argument: "$@"
+
+# "$@": ./filename1  arg1 ... argn,
+# where filename1 is a matlab executable compiled with mcc:
+
+# From the ccr linux front end machine:
+
+# module load gcc/10.2.0
+# module load matlab/R2019b
+# mcc -m filename1.m filname2.m ... filenamen.m -o filename1
+
+# mcc creates matlab executables to run on a platform corresponding to the platform on which they are generated
+
+echo "$@"
+module load gcc/10.2.0
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/util/academic/matlab/R2019b/bin/glnxa64
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/util/academic/matlab/R2019b/runtime/glnxa64
+export LD_LIBRARY_PATH
+#echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
+eval "$@"
