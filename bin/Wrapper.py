88ea0f4bfb96ce3662552d90708f592e0f95c49f
Renette Jones-Ivey <2087294+rljbufny1@users.noreply.github.com>
2023-07-19 23:38:16 -0400

    Mods to simplify Wrapper interface

--- bin/Wrapper.py
+++ bin/Wrapper.py
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------------------------------------------------------
 # Class: Wrapper
 # Component of: ghub_exercise1 (github.com)
-# Called from: Invoked as a thread from ghub_exercise1.ipynb
+# Called from: ghub_exercise1.ipynb
 # Purpose: Run a Pegasus workflow via the HUBzero hublib.cmd interface
 # Author: Renette Jones-Ivey
 # Date: Feb 2023
@@ -19,6 +19,7 @@ import hublib.cmd
 import DAX3 as DAX3
 #help (DAX3)
 
+'''
 import signal
 
 from newthreading import Thread
@@ -42,6 +43,7 @@ signal.signal(signal.SIGHUP, sig_handler)
 signal.signal(signal.SIGINT, sig_handler)
 signal.signal(signal.SIGQUIT, sig_handler)
 signal.signal(signal.SIGTERM, sig_handler)
+'''
 
 # Wrapper class
 # Called from ghub_exercise1.ipynb
@@ -79,47 +81,33 @@ class Wrapper():
 
         try:
         
-            #print ("Thread.rapptureCommandPid: ", Thread.rapptureCommandPid)
-    
-    
             #########################################################
-            # Paths to executables
+            # Create the Pegasus WMS workflow
             #########################################################
     
-            # Launch compiled Matlab executables.
-            # Note: cannot set the launch script to a script in a local directory,
-            # in this case, the error message:
-            # the selected site does not meet the specified application access requirements,
-            # is returned
+            # Create the workflow as an abstract DAG.
+    
+            dax = DAX3.ADAG("ghub_exercise3-workflow")
+            
+            # Add MATLAB launch script to the workflow's transformation catalog.
+            # Please see an example of a python launchscript in the /remotebin directory.
             
             matlab_launch_exec_path = os.path.join(self.tooldir, "remotebin", "matlabLaunch.sh")
             #print ("matlab_launch_exec_path : %s" %matlab_launch_exec_path)
     
-            e_matlab_launch = DAX3.Executable(namespace="ghub_exercise1-workflow", name="matlab-launch", \
+            e_matlab_launch = DAX3.Executable(namespace="ghub_exercise3-workflow", name="matlab-launch", \
                 os="linux", arch="x86_64", installed=False)
             e_matlab_launch.addPFN(DAX3.PFN("file://" + matlab_launch_exec_path , "local"))
             
-            #########################################################
-            #########################################################
-    
-            # Create the Pegasus workflow.
-            # Create an abstract DAG
-    
-            dax = DAX3.ADAG("ghub_exercise1-workflow")
-    
-            # Add launch script and input files to the DAX-level replica catalog
-    
             dax.addExecutable(e_matlab_launch)
             
-            # Original MATLAB file:
-            # Zenodo archive with doi: https://doi.org/10.5281/zenodo.3940768   
+            # Add input files to the workflow's replica catalog.
             
             # PFNs:
             # All files in a Pegasus workflow are referred to in the DAX using their Logical File Name (LFN). 
             # These LFNs are mapped to Physical File Names (PFNs) when Pegasus plans the workflow. 
-            
-            
-            # Job 1 matlab executable
+
+            # Job 1 input, compiled matlab executable
             filename = "deg2utm"
             filepath = os.path.join(self.bindir, filename)
             deg2utm =  DAX3.File(filename)
@@ -132,116 +120,101 @@ class Wrapper():
             utm_txt =  DAX3.File(filename)
             utm_txt.addPFN(DAX3.PFN("file://" + filepath, "local"))
             
-            # Job 2 matlab executable
+            # Job 2 input, compiled matlab executable
             filename = "utm2deg"
             filepath = os.path.join(self.bindir, filename)
             utm2deg =  DAX3.File(filename)
             utm2deg.addPFN(DAX3.PFN("file://" + filepath, "local"))
             dax.addFile(utm2deg)
             
-            # Job 1 output
+            # Job 2 output
             filename = "deg.txt"
             filepath = os.path.join(self.workingdir, filename)
             deg_txt =  DAX3.File(filename)
             deg_txt.addPFN(DAX3.PFN("file://" + filepath, "local"))
+
+            # Add jobs to the workflow.
             
-            # Create the FigurePaperFinals directory.
-            # Separate job to avoid parallel processor contingencies
-            
-            jobstep1 = DAX3.Job(namespace="ghub_exercise1-workflow", name="matlab-launch")
+            jobstep1 = DAX3.Job(namespace="ghub_exercise3-workflow", name="matlab-launch")
             jobstep1.addProfile(DAX3.Profile(DAX3.Namespace.GLOBUS,'maxwalltime', self.maxwalltime))
             jobstep1.addArguments("""./deg2utm %s %s""" %(self.latitude, self.longitude))
             jobstep1.uses(deg2utm, link=DAX3.Link.INPUT)
             jobstep1.uses(utm_txt, link=DAX3.Link.OUTPUT, transfer=True)
             dax.addJob(jobstep1)
             
-            jobstep2 = DAX3.Job(namespace="ghub_exercise1-workflow", name="matlab-launch")
+            '''
+            jobstep2 = DAX3.Job(namespace="ghub_exercise3-workflow", name="matlab-launch")
             jobstep2.addProfile(DAX3.Profile(DAX3.Namespace.GLOBUS,'maxwalltime', self.maxwalltime))
             jobstep2.addArguments("""./utm2deg""")
             jobstep2.uses(utm2deg, link=DAX3.Link.INPUT)
             jobstep2.uses(deg_txt, link=DAX3.Link.OUTPUT, transfer=True)
             dax.addJob(jobstep2)
-            # Depends on job step 1 completing
-            dax.addDependency(DAX3.Dependency(parent=jobstep1, child=jobstep2))
             
-          #########################################################
-            # Execute the workflow
+            # Job 2 depends on Job 1 completing
+            dax.addDependency(DAX3.Dependency(parent=jobstep1, child=jobstep2))
+            '''
+            
+            #########################################################
+            # Create the Pegasus Workflow DAX file
             #########################################################
     
-            dax_filename = "ghub_exercise1-workflow.dax"
+            dax_filename = "ghub_exercise3-workflow.dax"
             dax_filepath = os.path.join(self.workingdir,dax_filename);
             fp = open(dax_filepath, "w")
             if fp:
                 dax.writeXML(fp)
                 fp.close()
-                #print ("Wrapper.py thread: %s daxfile created successfully\n" %dax_filepath)
+                #print ("The daxfile %s created successfully\n" %dax_filepath)
             else:
-                print ("Wrapper.py thread: Could not create the %s daxfile\n" %dax_filepath)
+                print ("Wrapper.py thread: Could not create the daxfile %s\n" %dax_filepath)
                 #self.parent.handle_wrapper_error()
                 return
     
             # Verify contents
-            #fp = open("benthysplitworkflow.dax", "r")
+            #fp = open("ghub_exercise3-workflow.dax", "r")
             #file_contents = fp.read()
             #print (file_contents)
             #fp.close()
             
             sys.stdout.flush()
             
+            #########################################################
+            # Submit the Pegasus Workflow Plan
+            #########################################################
+    
             #'''
-            submitcmd = ["submit", "--venue", "WF-ccr-ghub", "pegasus-plan", "--dax", "ghub_exercise1-workflow.dax"]
+            submitcmd = ["submit", "--venue", "WF-ccr-ghub", "pegasus-plan", "--dax", "ghub_exercise3-workflow.dax"]
             #print ("submitcmd: ", submitcmd)
-    
-            # Fork to allow SIGTERM signal to get sent to RapptureExec
-            Thread.rapptureCommandPid = os.fork()
-            if (Thread.rapptureCommandPid >= 0):
-                if Thread.rapptureCommandPid == 0:
-                    # Reference: https://hubzero.org/documentation/1.3.0/tooldevs/grid.rappture_submit
-                    # child process.
-                    # RapptureExec messages redirected to the output log
-                    #print ("child branch: ");
-                    #print (Thread.rapptureCommandPid)
-                    # This waits until completion (blocks)
-                    exitCode,pegasusStdout,pegasusStderr = hublib.cmd.command.executeCommand(submitcmd,streamOutput=True)
-                    #print ("Wrapper.py thread: hublib.cmd.command.executeCommand(%s) exit code = %d\n" %(submitcmd, exitCode))
-                    if (exitCode == 0):
-                        os._exit(0)
-                    else:
-                        os._exit(exitCode)
-                else:
-                    # parent process.
-                    # Wait for child to complete
-                    #print ("wrapper.py thread: Waiting for workflow to complete...\n")
-                    print ("Waiting for the workflow to complete...\n")
-                    # Send message to the parent's parent to enable the abort button
-                    #self.parent.handle_wrapper_initialized()
-                    pid, rapptureExitCode = os.waitpid(Thread.rapptureCommandPid, 0)
-    
-                    Thread.rapptureCommandPid = -1
-                    if (rapptureExitCode != 0):
-                        # In this case, look for .stderr and .stdout files in the work directory
-                        print ("Wrapper.py thread: hublib.cmd.command.executeCommand(%s) returned with a non zero exit code = %d\n" %(submitcmd, rapptureExitCode))
-                        #self.parent.handle_wrapper_error()
-                        files = os.listdir(self.workingdir)
-                        files.sort(key=lambda x: os.path.getmtime(x))
-                        for file in files:
-                            # Get the numbered Pegasus work directory
-                            #print ('type(file): ', type(file)) #<class 'str'>
-                            if os.path.isfile(file) and file[0].isdigit() and file.endswith('.stderr'):
-                                print ("stderr file: %s\n" %os.path.join(self.workingdir, file))
-                                print ("For the ismip6aissvn tool, the following errors were returned while running a Pegasus workflow: ")
-                                with open(file) as f:
-                                    lines = f.readlines()                                
-                                    for line in lines:
-                                        if "WARNING" not in line:
-                                            print (line)
-                                # In case there is more than one stderr file in the working directory
-                                break          
-                        return
-            else:
-                print ("Wrapper.py thread: Failed to fork RapptureExec\n")
-                #self.parent.handle_wrapper_error()
+
+            # submit blocks.
+            exitCode,pegasusStdout,pegasusStderr = hublib.cmd.command.executeCommand(submitcmd,streamOutput=True)
+
+            if (exitCode == 0):
+
                 return
+
+            else:
+            
+                # In this case, look for .stderr and .stdout files in the work directory
+                print ("Wrapper.py: hublib.cmd.command.executeCommand(%s) returned with a non zero exit code = %d\n" %(submitcmd, exitCode))
+                #self.parent.handle_wrapper_error()
+                files = os.listdir(self.workingdir)
+                files.sort(key=lambda x: os.path.getmtime(x))
+                for file in files:
+                    # Get the numbered Pegasus work directory
+                    #print ('type(file): ', type(file)) #<class 'str'>
+                    if os.path.isfile(file) and file[0].isdigit() and file.endswith('.stderr'):
+                        print ("stderr file: %s\n" %os.path.join(self.workingdir, file))
+                        print ("For the ghubex3 tool, the following errors were returned while running a Pegasus workflow: ")
+                        with open(file) as f:
+                            lines = f.readlines()
+                            for line in lines:
+                                if "WARNING" not in line:
+                                    print (line)
+                        # In case there is more than one stderr file in the working directory
+                        break
+                return
+
     
             #'''
             print (" ")
