c9fd9d22ab28b0a839544f14e3fa6b04f52cc585
Renette Jones-Ivey <2087294+rljbufny1@users.noreply.github.com>
2023-11-12 17:38:58 -0500

    Update build_matlab_executables.sh

--- build_matlab_executables.sh
+++ build_matlab_executables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 
-# Compile Matlab executables on CCR for the Pegasus workflow
+# Compile Matlab executables on CCR for the Pegasus workflow.
+# Usage: login to CCR before running this script.
 
 # vortex
 module load gcc/10.2.0
@@ -9,28 +10,36 @@ module load matlab/R2019b
 #module load gcc/11.2.0
 #module load matlab/2021b
 
-# Note: for ../remotebin/matlabLaunch.sh LD_LIBRARY_PATH
+# Note for ../remotebin/matlabLaunch.sh LD_LIBRARY_PATH
 which gcc
 which matlab
 
-matlab_bin_dir='/projects/grid/ghub/Tools/software/2023.01/matlab/ghubex3/v1.0.0/bin'
-#echo ${matlab_bin_dir}
-mkdir -p ${matlab_bin_dir}
+rm -rf ./matlab_executables_build_dir
+git clone https://github.com/GhubGateway/Ghub_Pegasus_WMS_MATLAB_Example ./matlab_executables_build_dir
+cd ./matlab_executables_build_dir
 
 echo 'building deg2utm...'
-rm -f 'deg2utm.m'
-# Click the GitHub Raw button to get this link
-wget -q https://raw.githubusercontent.com/GhubGateway/Ghub_Pegasus_WMS_MATLAB_Example/main/src/deg2utm.m
-# mcc creates matlab executables to run on a platform corresponding to the platform on which they are generated
-mcc -m deg2utm.m -o deg2utm
-chmod +x deg2utm
-mv -f deg2utm ${matlab_bin_dir}
+# mcc creates MATLAB executables to run on a platform corresponding to the platform on which they are generated
+mcc -m ./src/deg2utm.m -o deg2utm
+mv deg2utm ./bin
 
 echo 'building utm2deg...'
-rm -f 'utm2deg.m'
-# Click the GitHub Raw button to get this link
-wget -q https://raw.githubusercontent.com/GhubGateway/Ghub_Pegasus_WMS_MATLAB_Example/main/src/utm2deg.m
-# mcc creates matlab executables to run on a platform corresponding to the platform on which they are generated
-mcc -m utm2deg.m -o utm2deg
-chmod +x utm2deg
-mv -f utm2deg ${matlab_bin_dir}
+# mcc creates MATLAB executables to run on a platform corresponding to the platform on which they are generated
+mcc -m ./src/utm2deg.m -o utm2deg
+mv utm2deg ./bin
+
+git add ./bin/deg2utm
+git update-index --chmod=+x ./bin/deg2utm
+git add ./bin/utm2deg
+git update-index ./bin/utm2deg
+git commit -m "Update compiled MATLAB executables"
+git status
+
+# You will be prompted for your GitHub user name and password.
+# Enter a personal access token for the password,
+# select the repo option (Full control of private repositories) when creating the personal access token.
+git config --global push.default matching
+git push
+
+cd ..
+rm -rf ./matlab_executables_build_dir
